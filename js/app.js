9cb0503bf0f2d3838539c256e17d3a9dd94be6dd
Mayra Cruz <mcruz@iMac-de-Administrador.local>
2019-08-05 19:11:42 -0500

    otra escena

--- js/app.js
+++ js/app.js
@@ -1,87 +1,29 @@
-'use strict'
-
-const canvas = document.querySelector('#demo')
-var renderer = new THREE.WebGLRenderer({
-  canvas
-});
-
-// const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
-
 var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-// renderer.setSize( window.clientWidth, window.clientHeight );
-// document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// var texture = new THREE.TextureLoader().load( '../assets/1.png' );
-
-
-const cuboMateriales = [
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/1.png' ) }),
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/2.png' ) }),
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/3.png' ) }),
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/4.png' ) }),
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/5.png' ) }),
-  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( '../assets/6.png' ) })
- ];
- 
- const material = new THREE.MeshFaceMaterial(cuboMateriales);
-
-// const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-const geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+camera.position.z = 5;
 
-var targetRotationY = 0;
-var targetRotationX = 0;
+var sphere = new THREE.SphereGeometry();
+	var object = new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( 0xff0000 ) );
+	var box = new THREE.BoxHelper( object, 0xffff00 );
+	scene.add( box );
 
 var animate = function () {
   requestAnimationFrame( animate );
 
-  
-  render()
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
 };
 
-const render = () => {
-  let canvas = renderer.domElement;
-  camera.aspect = canvas.clientWidth / canvas.clientHeight;
-  camera.updateProjectionMatrix();
-
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-  }
-
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
-  cube.rotation.y += ( targetRotationY - cube.rotation.y ) * 0.05;
-  // cube.rotation.x += ( targetRotationX - cube.rotation.x ) * 0.03;
-  renderer.render( scene, camera );  
-}
-
-function resizeRendererToDisplaySize(renderer) {
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-    renderer.setSize(width, height, false);
-  }
-  return needResize;
-}
-
-const handleMinutes = (e) => {
-  if(e.keyCode === 37) {
-    targetRotationY += 6;
-    // targetRotationX += 2;
-  } else {
-    return
-  }
-}
-
 animate();
-
-window.addEventListener('keydown', handleMinutes)
